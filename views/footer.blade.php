df0b6276e195468568d501fbc05bd18ba51a9a00
orlandoag11 <mv_morrito@hotmail.com>
2014-12-05 18:25:41 -0700

    cambios del login y colores del menú

--- views/footer.blade.php
+++ views/footer.blade.php
@@ -1,7 +1,7 @@
     <!-- /.container -->
     <!-- jQuery Version 1.11.0 -->
     <script src="<?=getPublic();?>/js/jquery-1.11.0.js"></script>
-
+    <script src="<?=getPublic();?>/js/jqBootstrapValidation.js"></script>
     <!-- Bootstrap Core JavaScript -->
     <script src="<?=getPublic();?>/js/bootstrap.min.js"></script>
     
--- views/login.blade.php
+++ views/login.blade.php
@@ -1,16 +1,14 @@
 <?php include("header.blade.php"); ?>
 <div class="container">
 
-<div class="row">
-    <div class="col-md-4" style="background:blue">;
-        
-    </div>
-</div>
+<div class="espacio-logo">
+<h1>FoodShot</h1>
+<div class="panel-login">
     <form class="form-horizontal" role="form">
         <div class="form-group">
-          <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+          <label for="inputEmail3" class="col-sm-2 control-label">Usuario</label>
             <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+              <input type="email" class="form-control" id="inputEmail3" placeholder="Usuario">
             </div>
         </div>
         <div class="form-group">
@@ -34,5 +32,7 @@
           </div>
         </div>
       </form>
+      </div>
+      </div>
 </div> 
 <?php include("footer.blade.php"); ?>
